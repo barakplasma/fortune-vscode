957b70854f6f56ed35299a6f1fa8d5ed9fe5c7bc
Michael Salaverry <barakplasma@gmail.com>
2019-07-05 20:29:50 +0300

    feat: display fortune on startup

--- src/extension.ts
+++ src/extension.ts
@@ -27,6 +27,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+	showFortune();
+
 	let disposable = vscode.commands.registerCommand('extension.displayFortune', () => {
 		// The code you place here will be executed every time your command is executed
 		showFortune();
