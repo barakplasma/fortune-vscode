14ff166046d02cc6fbe2a2d1a908f22bcfdcb759
Michael Salaverry <barakplasma@gmail.com>
2019-07-05 20:03:37 +0300

    feat: display another fortune from current fortune
    also displays the full fortune instead of a short piece

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,24 @@
 import * as vscode from 'vscode';
 import fortuneSource from 'fortune-tweetable';
 
+const showFortune = () => {
+	// The code you place here will be executed every time your command is executed
+	const aFortune = fortuneSource.fortune();
+
+	const MessageItems: vscode.MessageItem[] = [{ title: 'Show another' }]
+
+	// Display a message box to the user
+	vscode.window.showInformationMessage(
+		aFortune, {}, ...MessageItems
+	).then(
+		result => {
+			if (result === MessageItems[0]) {
+				showFortune()
+			}
+		}
+	);
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -11,13 +29,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.displayFortune', () => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage(fortuneSource.fortune());
+		showFortune();
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
